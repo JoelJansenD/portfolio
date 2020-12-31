83c4c4a045408877317e973ca457538d884b1dde
Joël Jansen Duighuizen <joeljansend@gmail.com>
2020-12-31 11:53:30 +0100

    Updates

---
+++ static/js/main.181d5f46.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{10:function(t,e,n){},11:function(t,e,n){"use strict";n.r(e);var c=n(0),o=n(1),r=n.n(o),i=n(3),s=n.n(i);n(9),n(10);var p=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("h1",{children:"Test"})})};s.a.render(Object(c.jsx)(r.a.StrictMode,{children:Object(c.jsx)(p,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.181d5f46.chunk.js.map
