573e66de286f4c89fa6fd646f8a0ff2a123ff012
Joël Jansen Duighuizen <joeljansend@gmail.com>
2020-12-31 11:57:42 +0100

    Further cleaning

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './style/App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1>Test</h1>    
     </div>
   );
 }
